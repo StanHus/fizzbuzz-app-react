184c667695e8b71fb1d141abbd6c65d1e718a43c
Stan <guseletovS@gmail.com>
2021-08-26 10:37:09 +0100

    fizzbuzz works

--- src/components/fizzBuzz.tsx
+++ src/components/fizzBuzz.tsx
@@ -1,31 +1,28 @@
-import {useState} from "react";
+import { useState } from "react";
 
-export default  function FizzBuzzCount (): JSX.Element {
+export default function FizzBuzzCount(): JSX.Element {
+  const [state, setState] = useState<number>(1);
 
-  const [state, setState] =
-  useState<(number | string)>(0);
-  
-  const [currentValues, newValues] =
-  useState<(number | string)[]>([0]);
+  const [currentValues, newValues] = useState<(number | string)[]>([0]);
 
   const count = () => {
     setState(state + 1);
 
-        //state isn't a number. What is it???
-    if (state % 3 ==0 ) {
-        setState("fizz")
+    if (state % 3 === 0 && state % 15 !== 0) {
+      newValues([...currentValues, "fizz"]);
+    } else if (state % 5 === 0 && state % 15 !== 0) {
+      newValues([...currentValues, "buzz"]);
+    } else if (state % 15 === 0) {
+      newValues([...currentValues, "fizzbuzz"]);
+    } else {
+      newValues([...currentValues, state]);
     }
-
-    newValues([
-      ...currentValues,
-      state,
-    ]);
-}
-    return (
-        <div>
-          <h1>FizzBuzz</h1>
-          <p>Sequence: {currentValues.join(", ")}</p>
-          <button onClick={count}>next</button>
-        </div>
-      );
+  };
+  return (
+    <div>
+      <h1>FizzBuzz</h1>
+      <p>Sequence: {currentValues.join(", ")}</p>
+      <button onClick={count}>next</button>
+    </div>
+  );
 }
